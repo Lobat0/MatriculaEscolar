3c9622092f138caab21bd7c9c2c1b3c138a36546
ricardO <sleiph@gmail.com>
2022-04-10 16:31:54 -0300

    adicionando uns arquivos de apoio

--- Matricula/Controllers/HomeController.cs
+++ Matricula/Controllers/HomeController.cs
@@ -1,55 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Matricula.Models;
+﻿using System.Web.Mvc;
 
 namespace Matricula.Controllers
 {
     public class HomeController : Controller
     {
-        private MatriculaEntities db = new MatriculaEntities();
-
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET Home/Login
-        public ActionResult Login()
-        {
-            ViewBag.Message = "Colé";
-
-            return View();
-        }
-        // POST: Home/Login
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public string Login(string nome, string senha)
-        {
-            var usuario = db.Usuario
-                    .Where(u => u.nome == nome)
-                    .FirstOrDefault();
-
-            if (usuario.senha == senha)
-            {
-                return "logado";
-            }
-
-            return "banido";
-        }
-
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Descrição da aplicação.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Nossa página de contatos.";
 
             return View();
         }
---
+++ Matricula/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using Matricula.Models;
+using System.Data;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Matricula.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly MatriculaEntities db = new MatriculaEntities();
+
+        // GET Login
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Colé";
+
+            return View();
+        }
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string Index(string nome, string senha)
+        {
+            var usuario = db.Usuario
+                    .Where(u => u.nome == nome)
+                    .FirstOrDefault();
+
+            if (usuario != null)
+            {
+                if (usuario.senha == senha)
+                {
+                    return "logado";
+                }
+            }
+
+            return "banido";
+        }
+    }
+}
