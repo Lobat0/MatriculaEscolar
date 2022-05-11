fba3ca0624785fe952fb083191c6ae94b2a6e5a7
ricardO <sleiph@gmail.com>
2022-05-10 21:22:28 -0300

    dexeusalvaraqui

---
+++ MatriculaAcademica/Controllers/CadastroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MatriculaAcademica.Controllers
+{
+    public class CadastroController : Controller
+    {
+        // GET: Cadastro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MatriculaAcademica/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MatriculaAcademica.Models;
+
+namespace MatriculaAcademica.Controllers
+{
+    public class LoginController : Controller
+    {
+        private MatriculaAcademicadbEntities1 db = new MatriculaAcademicadbEntities1();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string login, string senha)
+        {
+            try
+            {
+                var usuario = db.Usuario.Where(u => u.login == login).FirstOrDefault();
+
+                if (usuario != null)
+                {
+                    if (usuario.senha == senha)
+                    {
+                        Session["nome"] = usuario.login;
+                        Session["tipo"] = usuario.tipo;
+                        ViewBag.Status = "200";
+                        return View("Sucesso");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.Status = e;
+                return View("Erro");
+            }
+            ViewBag.Status = "501";
+            return View("Erro");
+        }
+    }
+}
