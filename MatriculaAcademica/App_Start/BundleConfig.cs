e577cf54dbcb7dd7a6c47c3b8d485b2983aadaba
ricardO <sleiph@gmail.com>
2022-05-22 16:30:22 -0300

    Atualizando o bootstrap

--- MatriculaAcademica/App_Start/BundleConfig.cs
+++ MatriculaAcademica/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MatriculaAcademica
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
