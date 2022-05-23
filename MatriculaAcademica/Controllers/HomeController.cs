87f21c725e38e0897bf5a15aa6f39cc87ad98d0b
TaylanPeixoto <taylanpeixotodasilva@gmail.com>
2022-05-23 13:50:24 -0300

    asobiando na palestra

--- MatriculaAcademica/Controllers/HomeController.cs
+++ MatriculaAcademica/Controllers/HomeController.cs
@@ -13,20 +13,6 @@ namespace MatriculaAcademica.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         // Sair
         public ActionResult Logout()
         {
